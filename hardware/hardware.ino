6aa33811edd22b06546acbf8509229940ee2e4a1
Your Name <you@example.com>
2024-08-04 22:37:31 -0700

    main: Switching to bts

--- hardware/hardware.ino
+++ hardware/hardware.ino
@@ -1,9 +1,11 @@
 const uint8_t pin_left_enl = 2;
 const uint8_t pin_left_enr = 3;
-const uint8_t pin_left_pwm = 4;
-const uint8_t pin_right_enl = 5;
-const uint8_t pin_right_enr = 6;
-const uint8_t pin_right_pwm = 7;
+const uint8_t pin_left_pwml = 4;
+const uint8_t pin_left_pwmr = 5;
+const uint8_t pin_right_enl = 6;
+const uint8_t pin_right_enr = 7;
+const uint8_t pin_right_pwml = 9;
+const uint8_t pin_right_pwmr = 8;
 
 const size_t serial_buffer_len = 1024;
 uint8_t serial_buffer[serial_buffer_len];
@@ -40,17 +42,21 @@ unsigned long kill_timeout_ms = 1000;
 void setup() {
   pinMode(pin_left_enl, OUTPUT);
   pinMode(pin_left_enr, OUTPUT);
-  pinMode(pin_left_pwm, OUTPUT);
-  digitalWrite(pin_left_enl, LOW);
-  digitalWrite(pin_left_enr, LOW);
-  digitalWrite(pin_left_pwm, LOW);
+  pinMode(pin_left_pwml, OUTPUT);
+  pinMode(pin_left_pwmr, OUTPUT);
+  digitalWrite(pin_left_enl, HIGH);
+  digitalWrite(pin_left_enr, HIGH);
+  analogWrite(pin_left_pwml, 0);
+  analogWrite(pin_left_pwmr, 0);
 
   pinMode(pin_right_enl, OUTPUT);
   pinMode(pin_right_enr, OUTPUT);
-  pinMode(pin_right_pwm, OUTPUT);
-  digitalWrite(pin_right_enl, LOW);
-  digitalWrite(pin_right_enr, LOW);
-  digitalWrite(pin_right_pwm, LOW);
+  pinMode(pin_right_pwml, OUTPUT);
+  pinMode(pin_right_pwmr, OUTPUT);
+  digitalWrite(pin_right_enl, HIGH);
+  digitalWrite(pin_right_enr, HIGH);
+  analogWrite(pin_right_pwml, 0);
+  analogWrite(pin_right_pwmr, 0);
 
   Serial.setTimeout(1);
   Serial.begin(115200);
@@ -61,8 +67,10 @@ void setup() {
 
 void loop() {
   if(millis() >= kill_timer) {
-    analogWrite(pin_pwm_left, 0);
-    analogWrite(pin_pwm_right, 0);
+    analogWrite(pin_left_pwml, 0);
+    analogWrite(pin_left_pwmr, 0);
+    analogWrite(pin_right_pwml, 0);
+    analogWrite(pin_right_pwmr, 0);
   }
 
   const size_t bytes_read = Serial.readBytes(serial_buffer, serial_buffer_len);
@@ -111,26 +119,24 @@ void loop() {
         }
 
         if(current_packet.dir_left == 0) {
-          digitalWrite(pin_left_enl, LOW);
-          digitalWrite(pin_left_enr, HIGH);
+          analogWrite(pin_left_pwml, current_packet.pwm_left);
+          analogWrite(pin_left_pwmr, 0);
         }
         else
         {
-          digitalWrite(pin_left_enl, HIGH);
-          digitalWrite(pin_left_enr, LOW);
+          analogWrite(pin_left_pwml, 0);
+          analogWrite(pin_left_pwmr, current_packet.pwm_left);
         }
-        analogWrite(pin_left_pwm, current_packet.pwm_left);
 
-        if(current_packet.dir_left == 0) {
-          digitalWrite(pin_left_enl, LOW);
-          digitalWrite(pin_left_enr, HIGH);
+        if(current_packet.dir_right == 0) {
+          analogWrite(pin_right_pwml, current_packet.pwm_right);
+          analogWrite(pin_right_pwmr, 0);
         }
         else
         {
-          digitalWrite(pin_right_enl, HIGH);
-          digitalWrite(pin_right_enr, LOW);
+          analogWrite(pin_right_pwml, 0);
+          analogWrite(pin_right_pwmr, current_packet.pwm_right);
         }
-        analogWrite(pin_right_pwm, current_packet.pwm_right);
 
         kill_timer = millis() + kill_timeout_ms;
         Serial.println("Received (" +
@@ -138,6 +144,7 @@ void loop() {
                      String(current_packet.pwm_left) + ", " +
                      String(current_packet.dir_right) + ", " +
                      String(current_packet.pwm_right) + ")");
+
         current_state = header_1;
         break;
     }
